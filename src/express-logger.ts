1421386469fdb2b33351c4e75f1c3cbc97371d9f
Scott Hutchings <yoshutch@users.noreply.github.com>
2021-04-07 08:51:39 -0600

    use default then if provided it will be overrwritten
    Co-authored-by: Gary Crye <GaryGSC@users.noreply.github.com>

--- src/express-logger.ts
+++ src/express-logger.ts
@@ -4,7 +4,7 @@ import DefaultLogger from '@byu-oit/logger'
 
 export function LoggerMiddleware (options?: PinoHttp.Options): HttpLogger {
   return PinoHttp({
-    logger: options?.logger ?? DefaultLogger(),
+    logger: DefaultLogger(),
     genReqId: req => {
       return req.headers['x-amzn-trace-id'] ?? randomUuid() // use the amazon trace id as the request id
     },
